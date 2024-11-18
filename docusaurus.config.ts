03d7190fa88385cd1eb146a37d84fa145c72134c
Afief <afief.farista@fore.coffee>
2024-11-19 00:07:36 +0700

    added deployment branch

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,8 +17,9 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'afiefsky', // Usually your GitHub org/user name.
+  projectName: 'docs', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
