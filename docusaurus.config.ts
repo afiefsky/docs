79593fd9e040018c340b510836a53f449ec25d1f
Afief <afief.farista@fore.coffee>
2024-11-19 00:43:26 +0700

    baseurl change

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://afiefsky.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '',
+  baseUrl: '/gh-docs/',
   trailingSlash: false,
 
   // GitHub pages deployment config.
